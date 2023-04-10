4c4d3df70c94afe6ec5df79179941faa6cb1c53b
paraizovinicius <95001119+paraizovinicius@users.noreply.github.com>
2023-04-09 21:49:03 -0300

    marioPosition

--- MarioJump/script.js
+++ MarioJump/script.js
@@ -1,27 +1,39 @@
 
-function init() {
-    const mario = document.querySelector('.mario');
-    
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
-    const jump = () => {
-        mario.classList.add('jump');
 
-        console.log(mario)
+const jump = () => {
+    mario.classList.add('jump');
 
-        setTimeout(() => {
-            mario.classList.remove('jump');
-        }, 500);
-    };
+    console.log(mario)
 
-    function addEventListenerToSpaceBar() {
-        document.addEventListener("keydown", function (event) {
-            if (event.code === "Space") {
-                jump();
-            }
-        });
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+};
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = window.getComputedStyle(mario).bottom;
+    console.log(marioPosition)
+
+    if (pipePosition < 120 && pipePosition > 50 && marioPosition < '110px'){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`
     }
+},10);
 
-    addEventListenerToSpaceBar();
+function addEventListenerToSpaceBar() {
+    document.addEventListener("keydown", function (event) {
+        if (event.code === "Space") {
+            jump();
+        }
+    });
 }
+addEventListenerToSpaceBar();
 
-init();
+
+
+
