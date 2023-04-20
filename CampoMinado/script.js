b17b106381193c37c7f240495f1d80659134fdf0
paraizovinicius <95001119+paraizovinicius@users.noreply.github.com>
2023-04-20 17:00:06 -0300

    novas funcionalidades

--- CampoMinado/script.js
+++ CampoMinado/script.js
@@ -8,3 +8,63 @@ function replaceCell(cell) {
     cell.appendChild(bomb);
 
 }
+
+function getRandomValues(array, quantity) { // Seleciona valores aleatórios dentre o nosso array com 'quantity' elementos
+    var shuffledArray = array.slice();
+    for (var i = shuffledArray.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = shuffledArray[i];
+        shuffledArray[i] = shuffledArray[j];
+        shuffledArray[j] = temp;
+    }
+
+    return shuffledArray.slice(0, quantity);
+}
+
+
+function setBombedCells() {
+    var table = document.querySelector(".GameBoard");
+    var cells = table.querySelectorAll("td");
+    var c = 0;
+
+    // Cria um array somente com o índice de todas as células da tabela
+    // o 'value' é desprezado neste caso
+    var cellIndexes = Array.from({ length: cells.length }, (_, index) => index); // essa função será aplicada para cada item do array
+
+    // Seleciona aleatoriamente 16 células da tabela
+    var randomCellsIndexes = getRandomValues(cellIndexes, 16); //Função getRandomValues
+    console.log(randomCellsIndexes)
+
+    // Define o evento onclick para cada uma das células selecionadas
+    randomCellsIndexes.forEach(index => {
+        cells[index].onclick = function () {
+            if (c == 0){
+            replaceCell(this);
+            c++;
+            showGameOver();
+            }
+        };
+    });
+}
+
+function clickCells(){
+    var table = document.querySelector(".GameBoard");
+    var cells = table.querySelectorAll("td");
+
+    
+}
+
+
+function showGameOver() {
+    var h1 = document.createElement("h1");
+    h1.textContent = "Game Over";
+    h1.classList.add("gameover");
+    document.body.appendChild(h1);
+  }
+
+
+
+setBombedCells();
+clickCells();
+
+
