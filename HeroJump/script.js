853392760a64cfde0d38e78fc4f120aacc1c67b2
paraizovinicius <95001119+paraizovinicius@users.noreply.github.com>
2023-04-30 20:12:28 -0300

    running functionallity

--- HeroJump/script.js
+++ HeroJump/script.js
@@ -4,38 +4,13 @@ const alturaDoPulo = 100;
 let posicao = 0;
 let subindo = false;
 
-
 var hero_moving = document.createElement("img");
 hero_moving.src = "mario.gif";
 
 var hero_still = document.createElement("img");
 hero_still.src = "marioparado.png";
 personagem.innerHTML = "";
-personagem.appendChild(hero_still);
-
-
-function moverParaDireita() {
-    
-    posicao += velocidade;
-    personagem.style.left = posicao + "px";
-    personagem.innerHTML = "";
-    personagem.appendChild(hero_moving);
-}
-
-function moverParaEsquerda() {
-    
-    posicao -= velocidade;
-    personagem.style.left = posicao + "px";
-    personagem.innerHTML = "";
-    personagem.appendChild(hero_moving);
-}
-
-function parar() {
-    personagem.innerHTML = "";
-    personagem.appendChild(hero_still);
-}
-
-
+personagem.appendChild(hero_still); // o personagem vai inicializar parado
 
 const pular = () => {
     personagem.classList.add('jump');
@@ -45,13 +20,67 @@ const pular = () => {
     }, 500);
 };
 
+let movimentoParaDireita = null;
+let movimentoParaEsquerda;
+let emMovimentoDireita = false;
+let emMovimentoEsquerda = false;
+
+
+function moverParaDireita() {
+    if (!emMovimentoDireita) {
+        emMovimentoDireita = true;
+  
+      movimentoParaDireita = setInterval(() => {
+        posicao += velocidade;
+        personagem.style.left = posicao + "px";
+        personagem.innerHTML = "";
+        personagem.appendChild(hero_moving);
+      }, 30);
+    }
+  
+    setTimeout(() => {
+      clearInterval(movimentoParaDireita);
+      emMovimentoDireita = false;
+    }, 3000);
+  }
+
+
+
+
+  function moverParaEsquerda() {
+    if (!emMovimentoEsquerda) {
+        emMovimentoEsquerda = true;
+  
+      movimentoParaEsquerda = setInterval(() => {
+        posicao -= velocidade;
+        personagem.style.left = posicao + "px";
+        personagem.innerHTML = "";
+        personagem.appendChild(hero_moving);
+      }, 30);
+    }
+  
+    setTimeout(() => {
+      clearInterval(movimentoParaEsquerda);
+      emMovimentoEsquerda = false;
+    }, 3000);
+  }
+
+
+function parar() {
+    clearInterval(movimentoParaDireita);
+    clearInterval(movimentoParaEsquerda);
+    personagem.innerHTML = "";
+    personagem.appendChild(hero_still);
+}
+
+
 document.addEventListener("keydown", function (event) {
     if (event.code === "ArrowRight") {
         moverParaDireita();
     } else if (event.code === "ArrowLeft") {
         moverParaEsquerda();
     } else if (event.code === "Space") {
-        pular(); 
+        pular();
     }
 });
 
